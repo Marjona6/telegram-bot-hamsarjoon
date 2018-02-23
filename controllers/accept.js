4eb30e43f273f925b5aa9e0d1d3e677f96b97588
Meryem Yanar <meryem.yanaryanar@gmail.com>
2018-02-23 17:16:53 +0300

    Make everything work except email

--- controllers/accept.js
+++ controllers/accept.js
@@ -4,25 +4,44 @@ const Telegram = require('telegram-node-bot');
 
 const form = require('../form');
 
+var checkKeyboard = {reply_markup: JSON.stringify({
+		keyboard: [
+			['بله'],
+			['نه']
+		],
+		keyboard_resize: true,
+		one_time_keyboard: true
+	})
+};
+
 class AcceptController extends Telegram.TelegramBaseController {
 	acceptHandler($) {
 		$.sendMessage('لطفا با دقت به پرسش ها ، پاسخ بدهید');
 		$.runForm(form, (result) => {
-			console.log(result);
-			$.sendMessage('Does this look right?');
+			result = JSON.parse(JSON.stringify(result));
+			// console.log(result);
+			$.sendMessage('آیا اطلاعات را تایید می کنید', checkKeyboard);
 			var userData = ``;
+			var userPhotoArray = [];
 			for (const prop in result) {
-				if (result.hasOwnProperty(prop) && typeof result.prop !== 'object') {
-					console.log(result.prop);
-					userData += `${result.prop}
+				console.log('Prop: ', prop, 'Result.prop: ', result[prop]);
+				if (result.hasOwnProperty(prop) && typeof result[prop] !== 'object') {
+					console.log(prop, result[prop]);
+					userData += `/${prop}: ${result[prop]}
 								`;
-					// $.sendMessage(result.prop);
-				} else if (result.hasOwnProperty(prop) && typeof result.prop === 'object') {
-					// $.sendPhoto(result.prop);
+				} else if (result.hasOwnProperty(prop) && typeof result[prop] === 'object') {
+					// add fileId to some array or...?
+					userPhotoArray.push(result[prop][0].file_id);
+					console.log(result[prop][0].file_id);
 				}
 			};
 			$.sendMessage(userData);
-			// $.sendMessage('عکس خود را بفرستید');
+			console.log('Photo array: ', userPhotoArray);
+			// send photo(s)
+			// loop through array
+			for (let i=0; i<userPhotoArray.length; i++) {
+				$.sendPhoto(userPhotoArray[i]);
+			}
 		});
 	}
 
