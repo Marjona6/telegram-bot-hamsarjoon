5db286123abd15d0b6fc8ce88d99a1a43f8690d0
Meryem Yanar <meryem.yanaryanar@gmail.com>
2018-03-13 18:44:58 +0300

    Add stuff for deployment

---
+++ bot.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const config = require('./config');
+const token = process.env.TOKEN || config.token;
+
+const Telegram = require('telegram-node-bot');
+var bot;
+// var bot = new Telegram.Telegram(token, {workers: 1});
+
+const PingController = require('./controllers/ping');
+const OtherwiseController = require('./controllers/otherwise');
+const StartController = require('./controllers/start');
+const NoAcceptController = require('./controllers/no-accept');
+const AcceptController = require('./controllers/accept');
+const ReceiveFileController = require('./controllers/receive-file');
+const EditDataController = require('./controllers/edit-data');
+const ConfirmDataController = require('./controllers/confirm-data');
+
+if(process.env.NODE_ENV === 'production') {
+  bot = new Telegram(token, {workers: 1});
+  bot.setWebHook(process.env.HEROKU_URL + bot.token);
+}
+else {
+  bot = new Telegram(token, { polling: true });
+}
+
+
+// Handling unhandled rejections
+process.on('unhandledRejection', error => {
+	console.log('unhandledRejection:', error.message);
+});
+
+// TODO: should not continue asking questions when user does not accept Ts&Cs
+
+bot.router.when(new Telegram.TextCommand('/ping', 'pingCommand'), new PingController())
+	// Start conversation with bot
+	.when(new Telegram.TextCommand('/start', 'startCommand'), new StartController())
+	// User does not accept terms and conditions
+	.when(new Telegram.TextCommand('عدم پذیرش قوانین موسسه همسرجون', 'noAcceptCommand'), new NoAcceptController())
+	// User accepts terms and conditions
+	.when(new Telegram.TextCommand('قبول می کنم', 'acceptCommand'), new AcceptController())
+	// User wants to edit input form data
+	.when(new Telegram.TextCommand('تایید نمی کنم', 'editDataCommand'), new EditDataController())
+	// User confirms that input data is correct (time to send the data!)
+	//.when(new Telegram.TextCommand('تایید می کنم', 'confirmDataCommand'), new ConfirmDataController())
+	.otherwise(new OtherwiseController());
+
+module.exports = bot;
--- index.js
+++ index.js
@@ -1,38 +1,4 @@
-"use strict";
+require("dotenv").config();
 
-const config = require('./config');
-const token = config.token;
-
-const Telegram = require('telegram-node-bot');
-const tg = new Telegram.Telegram(token, {workers: 1});
-
-const PingController = require('./controllers/ping');
-const OtherwiseController = require('./controllers/otherwise');
-const StartController = require('./controllers/start');
-const NoAcceptController = require('./controllers/no-accept');
-const AcceptController = require('./controllers/accept');
-const ReceiveFileController = require('./controllers/receive-file');
-const EditDataController = require('./controllers/edit-data');
-const ConfirmDataController = require('./controllers/confirm-data');
-
-// Handling unhandled rejections
-process.on('unhandledRejection', error => {
-	console.log('unhandledRejection:', error.message);
-});
-
-// TODO: should not continue asking questions when user does not accept Ts&Cs
-
-tg.router.when(new Telegram.TextCommand('/ping', 'pingCommand'), new PingController())
-	// Start conversation with bot
-	.when(new Telegram.TextCommand('/start', 'startCommand'), new StartController())
-	// User does not accept terms and conditions
-	.when(new Telegram.TextCommand('عدم پذیرش قوانین موسسه همسرجون', 'noAcceptCommand'), new NoAcceptController())
-	// User accepts terms and conditions
-	.when(new Telegram.TextCommand('قبول می کنم', 'acceptCommand'), new AcceptController())
-	// User wants to edit input form data
-	.when(new Telegram.TextCommand('تایید نمی کنم', 'editDataCommand'), new EditDataController())
-	// User confirms that input data is correct (time to send the data!)
-	//.when(new Telegram.TextCommand('تایید می کنم', 'confirmDataCommand'), new ConfirmDataController())
-	.otherwise(new OtherwiseController());
-
-// tg.addScopeExtension();
+var bot = require('./bot');
+require('./web')(bot);
